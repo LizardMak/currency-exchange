3c3518db4d894d95cf0ac797c551ed46b348efd9
Hunter Empey <LizardMak@gmail.com>
2023-11-17 10:47:17 -0700

    add API (wip)

--- src/submitHandler.js
+++ src/submitHandler.js
@@ -6,5 +6,22 @@ function handleSubmit() {
   event.preventDefault();
   let USDAmount = document.getElementById("amountUSD").value;
   let exchangeTo = document.querySelector('input[name="currency"]:checked').id;
-  
+  let exchangeRate = getExchangeRate(exchangeTo);
+  alert(USDAmount);
+  alert(exchangeRate);
+}
+
+function getExchangeRate(exchangeTo) {
+  return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${exchangeTo}`)
+  .then(function(response) {
+    if (!response.ok) {
+      const errorMessage = `${response.status} ${response.statusText}`;
+      throw new Error(errorMessage);
+    } else {
+      return response.json();
+    }
+  })
+  .catch(function(error) {
+    return error;
+  });
 }
