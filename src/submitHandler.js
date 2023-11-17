85240786522bf757b2688b2af14a4cb91e597343
Hunter Empey <LizardMak@gmail.com>
2023-11-17 09:40:23 -0700

    add submit

---
+++ src/submitHandler.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function () {
+  this.window.document.getElementById("form").addEventListener("submit", handleSubmit)
+})
+
+function handleSubmit() {
+  event.preventDefault();
+  
+}
