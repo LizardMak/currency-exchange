9da8a0345ca0aabc9e02835ab6a0a40b4210a476
Hunter Empey <LizardMak@gmail.com>
2023-11-17 17:28:32 -0700

    add error for fake currency

--- src/getExchangeRate.js
+++ src/getExchangeRate.js
@@ -4,8 +4,13 @@ export default class ExchangeRates {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}`);
       const responseJson = await response.json();
       if (!response.ok) {
-        const errorMessage = `${response.status}, search for 'error ${response.status} API' online for more details`;
-        throw new Error(errorMessage);
+        if (response.status === 404) {
+          const errorMessage = "Error: Currency does not exist!"
+          throw new Error(errorMessage)
+        } else {
+          const errorMessage = `${response.status}, search for 'error ${response.status} API' online for more details`;
+          throw new Error(errorMessage);
+        }
       } else {
         return responseJson;
       }
